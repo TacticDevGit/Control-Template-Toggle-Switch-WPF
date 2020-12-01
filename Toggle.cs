324b22aaee236a2913011c3741da305893bf8acc
Tactic Apps <38791845+TacticApps@users.noreply.github.com>
2020-12-01 20:44:36 +0200

    Add files via upload

---
+++ Toggle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToggleClass
+{
+
+    public class Toggle : ToggleButton
+    {
+
+        static Toggle()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Toggle), new FrameworkPropertyMetadata(typeof(Toggle)));
+        }
+    }
+}
